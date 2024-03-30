dca77f3a70769fab670fdb6f9ccfcdb846e84ede
matteo_andre <alphegon18@gmail.com>
2024-03-30 09:54:35 +0100

    feat(cms): (wip) Nav menu

--- perform-cms/src/App.vue
+++ perform-cms/src/App.vue
@@ -1,22 +1,76 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
-
+import image from '@/assets/logo_perform.png'
 </script>
 
 <template>
   <header>
-
-    <template>
-      <a href="/home">Home</a>
-      <a href="/users">Users</a>
-      <a href="/exercices">Exercices</a>
-      <a href="/materials">Materials</a>
-    </template>
-
-
+    <div class="navApp">
+      <div class="logoWrapper">
+        <img class="logo" :src="image" />
+      </div>
+      <nav>
+        <a href="/home">Home</a>
+        <a href="/users">Users</a>
+        <a href="/exercices">Exercices</a>
+        <a href="/materials">Materials</a>
+      </nav>
+      <div class="buttonWrapper">
+        <button>Disconnect</button>
+      </div>
+    </div>
   </header>
 
   <RouterView />
 </template>
+
+<style>
+.navApp {
+  height: 100%;
+  width: 300px;
+  padding-top: 10px;
+  box-shadow: 0px 0px 10px rgb(219, 219, 219);
+  position: relative;
+}
+
+.navApp nav {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+}
+
+.navApp nav a {
+  padding-left: 20px;
+  padding-right: 20px;
+  color: black;
+}
+
+.navApp .logo {
+  padding-left: 20px;
+  padding-right: 20px;
+  margin: 10px;
+  width: 200px;
+}
+
+.navApp .logoWrapper {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.navApp .buttonWrapper {
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.buttonWrapper button {
+  width: 100%;
+  height: 40px;
+}
+</style>
--- perform-cms/src/views/HomeView.vue
+++ perform-cms/src/views/HomeView.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-const router = useRouter();
-import { add } from 'ionicons/icons';
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <ion-page>
-    <ion-content>
-      <ion-list>
-        <ion-item>
-          <ion-checkbox slot="start"></ion-checkbox>
-          <ion-label>
-            <h1>Create Idea</h1>
-            <ion-note>Run Idea By Brandy</ion-note>
-          </ion-label>
-          <ion-badge color="success" slot="end"> 5 Days </ion-badge>
-        </ion-item>
-      </ion-list>
-      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
-        <ion-fab-button @click="() => router.push('/new')">
-          <ion-icon :icon="add"></ion-icon>
-        </ion-fab-button>
-      </ion-fab>
-    </ion-content>
-  </ion-page>
+  <div class="mainWrapper">
+    <h1>Home</h1>
+  </div>
+  <RouterView />
 </template>
