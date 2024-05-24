42d0401fdde7d83aaa774af621d603cb5afead51
Mattéo ANDRE <mandre@esrifrance.fr>
2024-05-24 16:33:16 +0200

    fix(deploy): fix caddy

--- prod.mk
+++ prod.mk
@@ -2,16 +2,16 @@ include ./config/.env.production
 export $(shell sed 's/=.*//' config/.env.production)
 
 build:
-	@docker compose --file docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production build --no-cache
+	@docker compose -f docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production build --no-cache
 
 run:
-	@docker compose --file docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production up -d
+	@docker compose -f docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production up -d
 
 stop:
-	@docker compose --file docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production stop
+	@docker compose -f docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production stop
 
 clean_containers:
-	@docker container prune --file
+	@docker container prune -f
     
 clean_images:
-	@docker image prune --file
+	@docker image prune -f
