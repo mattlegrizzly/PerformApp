d760fb10920beab8615c43bc4a1375bd0e671ef5
Mattéo ANDRE <mandre@esrifrance.fr>
2024-05-24 17:35:50 +0200

    fix(deploy): caddyfile + dockerprod

--- prod.mk
+++ prod.mk
@@ -1,17 +1,29 @@
 include ./config/.env.production
 export $(shell sed 's/=.*//' config/.env.production)
 
+ENV=config/.env.production
+API=django
+
 build:
-	@docker compose -f docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production build --no-cache
+	@docker compose -f docker-compose.production.yml -p perform-app --env-file config/.env.production build --no-cache
 
 run:
-	@docker compose -f docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production up -d
+	@docker compose -f docker-compose.production.yml -p perform-app --env-file config/.env.production up -d
+	mkdir -p /var/www/admin
+	@docker cp performapp-vue-admin-1:/var/www/admin/. /var/www/admin
+
 
 stop:
-	@docker compose -f docker-compose.production.yml -p grizzlee-recipes --env-file config/.env.production stop
+	@docker compose -f docker-compose.production.yml -p perform-app --env-file config/.env.production stop
 
 clean_containers:
 	@docker container prune -f
     
 clean_images:
 	@docker image prune -f
+
+createsuperuser:
+	@docker compose --f docker-compose.production.yml -p perform-app exec $(API) python manage.py createsuperuser
+
+init_db:
+	@docker compose -f docker-compose.production.yml -p perform-app --env-file config/.env.production exec -u $(DB_USER) postgres psql $(DB_NAME) $(DB_USER) -f /docker-entrypoint-initdb.d/init.sql
