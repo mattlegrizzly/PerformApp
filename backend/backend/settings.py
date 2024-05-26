d2653c86fbc48d18e6a0cdd79b0eaabb6574534c
Mattéo <mattbercegeay@gmail.com>
2024-05-26 15:27:34 +0200

    feat(backend): envoi du mail à l'enregistrement d'un utilisateur

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,10 +30,11 @@ DEBUG = True
 
 ADMIN_ENABLED = False
 ALLOWED_HOSTS=os.getenv("ALLOWED_HOSTS").split(",")
+#ALLOWED_HOSTS=['localhost']
 API_URL = os.getenv("API_URL")
 AUTH_USER_MODEL = "cms.User"
 #CORS_ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS").split(",")
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['http://localhost:5174']
 DEBUG = bool(os.getenv("DEBUG"))
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LANGUAGE_CODE = "en-us"
@@ -68,6 +69,12 @@ SIMPLE_JWT = {
     'SIGNING_KEY': SECRET_KEY,
 }
 
+SENDGRID_API_KEY = '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'
+
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+SENDGRID_ECHO_TO_STDOUT = True
+DEFAULT_FROM_EMAIL = 'contact@grizzlyperform.app'
 
 SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -93,6 +100,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'rest_auth.registration',
     'django_extensions',
+    'sendgrid',
     'users2',
     'sport',
     'exercise'
