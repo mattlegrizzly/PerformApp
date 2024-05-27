d16e66fc6b313d216a2a54897a1c629b2efc5111
Mattéo ANDRE <mandre@esrifrance.fr>
2024-05-28 00:02:11 +0200

    feat(app): page view

---
+++ perform-app/src/views/Login/LoginPage.vue
@@ -0,0 +1,15 @@
+<style scoped></style>
+
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="underTitle">CONNEXION</div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonPage } from "@ionic/vue";
+import "@/assets/base.css";
+import "@/assets/main.css";
+</script>
